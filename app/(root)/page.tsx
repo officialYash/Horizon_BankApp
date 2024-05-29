c43a9e0f950587ce09be5ab763c3f93def98e792
officialYash <38553961+officialYash@users.noreply.github.com>
2024-05-29 13:24:53 +0530

    Update Total Bank balance layout Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const Home = () => {
-  return (
-    <div>Home</div>
+ const loggedIn = {firstName: 'OfficialYash'}
+    return (
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+               <HeaderBox type="greeting" title="Welcome" user={
+                loggedIn?.firstName || 'Guest'} subtext="Access and Manage your account and transaction efficiently."
+               />
+               <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1350.35} />
+            </header>
+        </div>
+    </section>
   )
 }
 
