d4b8659741e5bbc0d857b1757d1446d6de4f7c1b
officialYash <38553961+officialYash@users.noreply.github.com>
2024-05-30 15:46:37 +0530

    Create Left sidebar layout section

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,15 @@
+import Sidebar from "@/components/Sidebar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = {firstName :'Yash' , lastName:'Arora'}
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user ={loggedIn}/>
         {children}
     </main>
   );
