67e38a77bd2ce30c774e3cf7563653ee02b6b505
officialYash <38553961+officialYash@users.noreply.github.com>
2024-06-03 13:12:40 +0530

    create bankcard layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const Home = () => {
- const loggedIn = {firstName: 'OfficialYash'}
+ const loggedIn = {firstName: 'OfficialYash', lastName:'Arora', email:'G4bru22@gmail.com'};
     return (
     <section className='home'>
         <div className='home-content'>
@@ -12,7 +13,9 @@ const Home = () => {
                />
                <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1350.35} />
             </header>
+            recent transaction
         </div>
+                <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance:123.50},{currentBalance:500.50}]}/>
     </section>
   )
 }
